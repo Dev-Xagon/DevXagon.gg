fdc22f92295d977ccc428b43d9ad611d250f2f66
Xagon <105147138+Dev-Xagon@users.noreply.github.com>
2025-10-09 14:55:33 +0200

    Add files via upload

---
+++ Taschenrechner/scriptTaschenrechner.js
@@ -0,0 +1,42 @@
+const ersteNummerInput= document.getElementById('ersteNummer'); //undefined
+const zweiteNummerInput= document.getElementById('zweiteNummer'); //undefined
+const operatorSelectInput=document.getElementById('operator');
+const berechnenButton= document.getElementById('berechnen');
+const resultatParagraph=document.getElementById('Resultat');
+
+const resultatImage=document.getElementById("resultatImage");
+
+let resultat;
+
+
+function berechnen()
+{
+const ersteNummer = parseFloat(ersteNummerInput.value);
+const zweiteNummer= parseFloat(zweiteNummerInput.value);
+const operator =operatorSelectInput.value;
+
+let resultat;
+
+switch(operator) {
+    case "+":
+        resultat = ersteNummer + zweiteNummer;
+        break;
+    case "-":
+        resultat = ersteNummer - zweiteNummer;
+        break;
+    case "*":
+        resultat = ersteNummer * zweiteNummer;
+        break;
+    case "/":
+        resultat = zweiteNummer !== 0 ? ersteNummer / zweiteNummer : "Durch 0 kann nicht geteilt werden!";
+        break;
+    default:
+        resultat = "Ungültiger Operator";
+}
+resultatParagraph.textContent ='Ergebnis: ' + resultat;
+
+resultatImage.textContent= resultat;
+}
+
+
+berechnenButton.addEventListener('click', berechnen);
